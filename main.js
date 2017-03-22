b271e7d9962c20b62c100ce3c5f830f709e58636
purlantov <ppurlantov@yahoo.com>
2017-03-22 19:00:58 +0200

    First block render commit

--- main.js
+++ main.js
@@ -1,1 +1,267 @@
-console.log('Hope everything is going to be fine!');
+function get(id) {
+    return document.getElementById(id);
+}
+
+// if (!window.requestAnimationFrame) {
+//     // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+//     window.requestAnimationFrame = window.webkitRequestAnimationFrame ||
+//         window.mozRequestAnimationFrame ||
+//         window.oRequestAnimationFrame ||
+//         window.msRequestAnimationFrame ||
+//         function (callback, element) {
+//             window.setTimeout(callback, 1000 / 60);
+//         }
+// }
+
+var canvas = get('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width  = 350;
+canvas.height = 600;
+ctx.scale(20, 20);
+
+var DIR = {
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 2,
+    LEFT: 3,
+    MIN: 0,
+    MAX: 3
+};
+
+t = {
+    size: 3,
+    color: 'purple',    
+    blocks: [
+        [
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 1, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 1, 1, 1],
+            [0, 0, 1, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 1, 1, 1],
+            [0, 0, 0, 0]
+        ]
+    ]
+}
+
+j = {
+    size: 3,
+    color: 'blue',    
+    blocks: [
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 1],
+            [0, 0, 0, 1],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 1],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 0, 0],
+            [0, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    ]
+}
+
+l = {
+    size: 3,
+    color: 'orange',    
+    blocks: [
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 1],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 1],
+            [0, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    ]
+}
+
+z = {
+    size: 3,
+    color: 'red',    
+    blocks: [
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 1],
+            [0, 0, 1, 1],
+            [0, 0, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 1],
+            [0, 0, 1, 1],
+            [0, 0, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    ]
+}
+
+s = {
+    size: 3,
+    color: 'green',    
+    blocks: [
+        [
+            [0, 0, 0, 0],
+            [0, 0, 1, 1],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 0, 1, 1],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1],
+            [0, 0, 0, 0]
+        ]
+    ]
+}
+
+i = {
+    size: 3,
+    color: '#ADD8E6',    
+    blocks: [
+        [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0]
+        ]
+    ]
+}
+
+o = {
+    size: 2,
+    color: 'yellow',
+    blocks: [
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    ]
+}
+
+
+function drawBlock(block, k, offsetX, offsetY) {
+    block.blocks[k].forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                ctx.fillStyle = block.color;
+                ctx.fillRect(x + offsetX, y + offsetY, 1, 1);
+            }
+        });
+    });
+}
+
+
+drawBlock(t, 2, 0, 0);
+drawBlock(j, 2, 3, 3);
+drawBlock(l, 2, 6, 6);
+drawBlock(z, 2, 2, 6);
+drawBlock(s, 2, 12, 12);
+drawBlock(i, 2, 2, 12);
+drawBlock(o, 2, 5, 20);

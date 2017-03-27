2117857eabc82536a92d37b93313728745f936de
Sevdalin Zhelyazkov <irrealizable@abv.bg>
2017-03-27 04:55:08 +0300

    Fixed bug: Player couldn't level up due to condition error; Fixed Score/Best coloring bug; Added Level Up message with timeout function

--- Tetris_v2/js/scores.js
+++ Tetris_v2/js/scores.js
@@ -7,23 +7,29 @@ function Player() {
 
 let currentPlayer = new Player();
 let bestScore = 0;
-let totalScore = document.getElementById('totalScore');
-let bestScoreHolder = document.getElementById('bestScore');
+let totalScore = document.getElementById('totalScore'),
+    bestScoreHolder = document.getElementById('bestScore'),
+    levelUp = document.getElementById('level-up');
 
 function updateScore() {
     currentPlayer.score += currentPlayer.increasePoints;
 
     if (currentPlayer.score > bestScore) {
+        totalScore.className = '';
         totalScore.classList.add('green');
     } else if (currentPlayer.score < bestScore) {
+        totalScore.className = '';
         totalScore.classList.add('red');
     }
     totalScore.innerText = currentPlayer.score;
 
-    if (currentPlayer.score === currentPlayer.scoreToLevelUp) {
+    if (currentPlayer.score >= currentPlayer.scoreToLevelUp) {
         currentPlayer.increasePoints += 20;
         currentPlayer.scoreToLevelUp = currentPlayer.scoreToLevelUp * 2;
+        console.log(currentPlayer.scoreToLevelUp);
         dropInterval = dropInterval - 10;
+        levelUp.innerHTML = 'Level up!<br />Points will increase with ' + currentPlayer.increasePoints;
+        setTimeout(function(){levelUp.innerHTML = '';}, 5000);
     }
 }
 

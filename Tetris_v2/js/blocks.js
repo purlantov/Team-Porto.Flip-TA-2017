0f030eb19d07896dccd05b20401500cdbdff8074
Nad Hr <epohster@gmail.com>
2017-03-29 18:49:37 +0300

    Blocks initial rotation matches next piece

--- Tetris_v2/js/blocks.js
+++ Tetris_v2/js/blocks.js
@@ -16,9 +16,9 @@ function Piece(type) {
 function createPieceMatrix(type) {
     if (type === 'T') {
         return [
-            [0, 0, 0],
+            [0, 1, 0],
             [1, 1, 1],
-            [0, 1, 0]
+            [0, 0, 0]
         ];
     } else if (type === 'O') {
         return [
@@ -27,15 +27,15 @@ function createPieceMatrix(type) {
         ];
     } else if (type === 'L') {
         return [
-            [0, 3, 0],
-            [0, 3, 0],
-            [0, 3, 3]
+            [0, 0, 3],
+            [3, 3, 3],
+            [0, 0, 0]
         ];
     } else if (type === 'J') {
         return [
-            [0, 4, 0],
-            [0, 4, 0],
-            [4, 4, 0]
+            [4, 0, 0],
+            [4, 4, 4],
+            [0, 0, 0]
         ];
     } else if (type === 'I') {
         return [
